b4c5c92892e56630917f7e442448e904ef53dbd7
Fengyun <shaoxt@gmail.com>
2019-04-27 10:47:13 -0700

    AbstractLifecycle

--- util/src/main/java/org/femtoframework/util/thread/LifecycleThread.java
+++ util/src/main/java/org/femtoframework/util/thread/LifecycleThread.java
@@ -1,16 +1,13 @@
 package org.femtoframework.util.thread;
 
-import org.femtoframework.bean.BeanPhase;
-import org.femtoframework.bean.LifecycleMBean;
-import org.femtoframework.bean.Nameable;
-import org.femtoframework.bean.NamedBean;
+import org.femtoframework.bean.*;
 import org.femtoframework.bean.annotation.Ignore;
 import org.femtoframework.bean.exception.StopException;
 
 /**
  * An thread + lifecycle adapter
  */
-public abstract class LifecycleThread implements LifecycleMBean, Runnable, NamedBean, Nameable {
+public abstract class LifecycleThread extends AbstractLifecycle implements Runnable, NamedBean, Nameable {
 
     private String name;
 
@@ -28,8 +25,6 @@ public abstract class LifecycleThread implements LifecycleMBean, Runnable, Named
      * Error Handler
      */
     protected ErrorHandler handler = null;
-
-    private BeanPhase beanPhase = BeanPhase.DISABLED;
 
     public boolean isDaemon()
     {
@@ -49,23 +44,6 @@ public abstract class LifecycleThread implements LifecycleMBean, Runnable, Named
         this.daemon = daemon;
     }
 
-    /**
-     * Implement method of getPhase
-     *
-     * @return BeanPhase
-     */
-    public BeanPhase _doGetPhase() {
-        return beanPhase;
-    }
-
-    /**
-     * Phase setter for internal
-     *
-     * @param phase BeanPhase
-     */
-    public void _doSetPhase(BeanPhase phase) {
-        this.beanPhase = phase;
-    }
 
     public void _doInit()
     {
